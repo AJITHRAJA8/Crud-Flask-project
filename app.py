f485de0a39e6e4ca3bec545bab91b502f87acde4
AJITHRAJA8 <ajithrajar79@gmail.com>
2026-06-05 14:12:06 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,redirect
 import mysql.connector
 
 #Getting File Name
@@ -8,7 +8,7 @@ app=Flask(__name__)
 con=mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "@Ajith@9751",
+    password = "Your_pass",
     database = "crud"
 )
 if con.is_connected():
@@ -18,7 +18,7 @@ else:
 
 #Loading Home Page
 @app.route('/')
-def Home():
+def home():
     res=con.cursor(dictionary=True)
     sql="select * from users"
     res.execute(sql)
@@ -28,7 +28,22 @@ def Home():
 #new user Insert
 @app.route('/add_user',methods=['GET','POST'])
 def add_user():
+    if request.method=='POST':
+        name=request.form['name']
+        age=request.form['age']
+        city=request.form['city']
+        res=con.cursor(dictionary=True)
+        sql="insert into users(name,age,city)values(%s,%s,%s)"
+        values=(name,age,city)
+        res.execute(sql,values)
+        con.commit()
+        return redirect(url_for('home'))
     return render_template("add_user.html")
+
+#update user using ID
+@app.route('/update',methods=['GET','POST'])
+def update():
+    return render_template("update")
 
 
 if (__name__=="__main__"):
